e65eb26d5ee6c0f6c57954aae1fa361086f6dcfa
Onkar2087 <114167896+Onkar2087@users.noreply.github.com>
2026-05-06 16:13:30 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.VITE_API_URL;
+const BACKEND_URL= `${import.meta.env.VITE_API_URL}/api`
 
 export const addLink = async (url) => {
     const res = await axios.post(`${BACKEND_URL}/add`, { url });
